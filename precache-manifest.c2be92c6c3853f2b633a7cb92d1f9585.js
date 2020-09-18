dd0976fdf5e1999f300fbb98c6e4ae7dd4d5c8e0
Andrey Kudryavtsev <lomastering@gmail.com>
2020-09-18 14:47:55 +0300

    Updates

---
+++ precache-manifest.c2be92c6c3853f2b633a7cb92d1f9585.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da8bfe2bb47f06069e6a721c4e75ba9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "cb998d2c21c71b66f00e",
+    "url": "/random-quote-machine/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cb359d15910c39893543",
+    "url": "/random-quote-machine/static/css/main.58074688.chunk.css"
+  },
+  {
+    "revision": "cb998d2c21c71b66f00e",
+    "url": "/random-quote-machine/static/js/2.89e6b8a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/random-quote-machine/static/js/2.89e6b8a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb359d15910c39893543",
+    "url": "/random-quote-machine/static/js/main.aefaa11f.chunk.js"
+  },
+  {
+    "revision": "81a81eeacc763523a4c2",
+    "url": "/random-quote-machine/static/js/runtime-main.d8166d98.js"
+  }
+]);
