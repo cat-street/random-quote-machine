519307fba4fc84a9a617b0e307a40bb166a48ed2
Andrey Kudryavtsev <lomastering@gmail.com>
2020-10-04 11:03:10 +0300

    Updates

--- precache-manifest.4538799de81301c90b084928b04f04e2.js
+++ precache-manifest.4538799de81301c90b084928b04f04e2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0e9bf0d4052cd5b2bbd65058ce6e5e0d",
+    "revision": "259460d682435cf6bd80e82a378e0d61",
     "url": "/random-quote-machine/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/random-quote-machine/static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "9f8aa157bd8cb678446a",
-    "url": "/random-quote-machine/static/css/main.de26370f.chunk.css"
+    "revision": "865edc4c7f0eff648769",
+    "url": "/random-quote-machine/static/css/main.5e71326c.chunk.css"
   },
   {
     "revision": "cb998d2c21c71b66f00e",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/random-quote-machine/static/js/2.89e6b8a7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f8aa157bd8cb678446a",
-    "url": "/random-quote-machine/static/js/main.a421a4a4.chunk.js"
+    "revision": "865edc4c7f0eff648769",
+    "url": "/random-quote-machine/static/js/main.783c898b.chunk.js"
   },
   {
     "revision": "81a81eeacc763523a4c2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/random-quote-machine/precache-manifest.e8a1fa72496c3372697ef78459cda84b.js"
+  "/random-quote-machine/precache-manifest.4538799de81301c90b084928b04f04e2.js"
 );
 
 self.addEventListener('message', (event) => {
